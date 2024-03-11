62f27cc3e8dac4fbff700b4135ea8753136442bf
Eryan GAUVRIT <eryan.gauvrit@gmail.com>
2024-03-11 17:22:11 +0100

    Add types

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.testPackage = exports.FastLogsService = void 0;
+exports.FastLogsService = void 0;
 var fastLogsService_1 = require("./fastLogsService");
 Object.defineProperty(exports, "FastLogsService", { enumerable: true, get: function () { return fastLogsService_1.FastLogsService; } });
-var testPackage = function () {
-    console.log('testPackage');
-};
-exports.testPackage = testPackage;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export { Action, FastLog, FastLogFilter, FastLogGet, FastLogPost, FastLogsService, Source } from "./fastLogsService";
+import { Action, FastLog, FastLogFilter, FastLogGet, FastLogPost, FastLogsService, Source } from "./fastLogsService";
+
+export { Action, FastLog, FastLogFilter, FastLogGet, FastLogPost, FastLogsService, Source };
+
